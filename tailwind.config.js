ad53da75d174fdda46693f26f4caaeecef7a89d3
Frida Arcadia <frida.arcadialv@gmail.com>
2026-03-29 20:16:55 -0600

    feat(atomicDesign): Crea atomos de íconos

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Front/src/**/*.{html,js}", "./Front/public/views/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
