43476712dade61e435f8f0f8ba878d9b9b17d2a4
Frida Arcadia <frida.arcadialv@gmail.com>
2026-04-02 23:42:56 -0600

    feat(login): Agrega logo y título a la página

--- Back/src/presentation/controller/AuthController.js
+++ Back/src/presentation/controller/AuthController.js
@@ -3,6 +3,27 @@ class AuthController {
         this.logoutUseCase = logoutUseCase;
     }
 
+    getLogin (req, res) {
+        try {
+            const message = req.session.info || "";
+            if (req.session.info) {
+                req.session.info = "";
+            }
+
+            const warning = req.session.warning || "";
+            if (req.session.warning) {
+                req.session.warning = "";
+            }
+
+            
+
+        } catch (error) {
+            return res.status(400).json({
+                error: error.message,
+            });
+        }
+    }
+
     logout (req, res) {
         try {
             const token = req.headers.authorization?.split(" ")[1];
