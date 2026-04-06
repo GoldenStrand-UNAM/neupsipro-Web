b602e2d3c69e40419d428fcd10cc6e8810e452d2
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-04-06 17:11:42 -0600

    feat(): Index js test

--- Back/src/index.js
+++ Back/src/index.js
@@ -1,22 +1,24 @@
-const express = require("express");
-const cors = require("cors");
-
-const AuthService = require("./Infrastructure/Auth/AuthService");
-const LogoutUseCase = require("./application/Usecase/LogoutUseCase");
-const AuthController = require("./Presentation/Controller/AuthController");
-const authRoutes = require("./Presentation/routes/authRoutes");
-
+const express = require('express');
+const cors = require('cors');
 const app = express();
+const path = require('path');
+ 
+// Middlewares
 app.use(cors());
 app.use(express.json());
-
-const authService = new AuthService();
-const logoutUseCase = new LogoutUseCase(authService);
-const authController = new AuthController(logoutUseCase);
-
-app.use("/auth", authRoutes(authController));
-
-app.listen(3000, () => {
-    // eslint-disable-next-line no-console
-    console.log("Server running on http://localhost:3000");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../../Front/public')));
+ 
+// Routes
+const usuarioRoutes = require('./presentation/routes/users/GetUsersList.Routes');
+app.use('/', usuarioRoutes);
+ 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'presentation/views/GetUsers.html'));
 });
+ 
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+ 
