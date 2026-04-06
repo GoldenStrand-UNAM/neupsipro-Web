0de79a3f017aea326fdb18037a6d0b18681bc7a9
Andrey <enriqueandrey@hotmail.com>
2026-04-06 01:06:20 -0600

    feat(Forum): back completed, fix sql query and index.js

--- Back/src/index.js
+++ Back/src/index.js
@@ -1,22 +1,29 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-require('dotenv').config();
-
 const path = require('path');
 
 
+require('dotenv').config();
+
+
 //Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname, '../../Front/public')));
 
 //Routes
 
 const forumRoutes = require('./presentation/routes/forum/getForum.routes');
-app.use('/forum', forumRoutes);
+app.use('/', forumRoutes);
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../Front/public/views/base-page.html'));
+});
 
-const PORT = 3306;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
