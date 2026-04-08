64aa6dc07927a050331905aa10e7fd7398af6721
Frida Arcadia <frida.arcadialv@gmail.com>
2026-04-08 12:12:53 -0600

    feat(login): uninspect .spect.js

--- Back/src/index.js
+++ Back/src/index.js
@@ -3,9 +3,6 @@ require("dotenv").config();
 const app = require ('./app.js');
 
 
-app.get('/test', (req, res) => {
-    res.render('test');
-});
 
 app.listen(3000, () => {
     // eslint-disable-next-line no-console
