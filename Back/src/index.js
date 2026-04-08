fc0f397648157a73b2b1230880142c2c0ed53bfa
nopusmiau <ricardoandraderobledo@gmail.com>
2026-04-08 11:47:03 -0600

    feat(login): Added structure for testing for this user story.

--- Back/src/index.js
+++ Back/src/index.js
@@ -20,6 +20,10 @@ const authController = new AuthController(logoutUseCase);
 
 app.use("/auth", authRoutes(authController));
 
+app.get('/test', (req, res) => {
+    res.render('test');
+});
+
 app.listen(3000, () => {
     // eslint-disable-next-line no-console
     console.log("Server running on http://localhost:3000");
