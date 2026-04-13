135effd5a41788a6f7a88e2e3a2b740d1d7dc3da
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-04-13 10:41:07 -0600

    fix: resolve casing issues and normalize controllers/routes

--- Back/src/presentation/routes/users/getUsersList.Routes.js
+++ Back/src/presentation/routes/users/getUsersList.Routes.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const UserRepository = require("../../../infrastructure/repositories/usersRepository");
 const GetUsersSummaryUseCase = require("../../../application/usecase/getUsersSummaryUseCase");
-const GetUsersListController = require("../../controller/users/GetUsersList.Controller");
+const getUsersListController = require("../controller/users/getUsersList.Controller");
 
 const repository = new UserRepository();
 const useCase = new GetUsersSummaryUseCase(repository);
-const controller = new GetUsersListController(useCase);
+const controller = new getUsersListController(useCase);
 
 router.get("/usuarios", (req, res) => controller.getUsersPage(req, res));
 router.get("/api/usuarios", (req, res) => controller.getUsers(req, res));
