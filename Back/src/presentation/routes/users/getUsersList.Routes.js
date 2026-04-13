19a6f8f69103af59484bd2237f8d085dd3a148b3
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-04-12 23:07:56 -0600

    fix(UserList): changed name of getUserList roure/controller

--- Back/src/presentation/routes/users/getUsersList.Routes.js
+++ Back/src/presentation/routes/users/getUsersList.Routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const UserRepository = require("../../../infrastructure/repositories/usersRepository");
-const GetUsersListUseCase = require("../../../application/usecase/users/getUserListUseCase");
+const GetUsersSummaryUseCase = require("../../../application/usecase/getUsersSummaryUseCase");
 const GetUsersListController = require("../../controller/users/GetUsersList.Controller");
 
 const repository = new UserRepository();
-const useCase = new GetUsersListUseCase(repository);
+const useCase = new GetUsersSummaryUseCase(repository);
 const controller = new GetUsersListController(useCase);
 
 router.get("/usuarios", (req, res) => controller.getUsersPage(req, res));
