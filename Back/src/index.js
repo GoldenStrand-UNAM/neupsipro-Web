261c3fc285ce504d927bb1c7a97206a509a21c4a
Andrey <enriqueandrey@hotmail.com>
2026-04-14 11:00:12 -0600

    fix: diveded index responsabilities and added app.js

--- Back/src/index.js
+++ Back/src/index.js
@@ -1,34 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const path = require('path');
 
+const app = require('./app');
 
-require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 
-
-//Middlewares
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use(express.static(path.join(__dirname, '../../Front/public')));
-
-//EJS 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../../Front/views'));
-
-//Routes
-app.use((req, res, next) => {
-    res.locals.activePage = '';
-    next();
-});
-
-const forumRoutes = require('./presentation/routes/forum/getForum.routes');
-app.use('/', forumRoutes);
-
-
-const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
