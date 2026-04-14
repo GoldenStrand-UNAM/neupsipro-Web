7281e2951ea2e1748e7172940eebc898e6aa1636
Andrey <enriqueandrey@hotmail.com>
2026-04-14 11:00:12 -0600

    fix: diveded index responsabilities and added app.js

---
+++ Back/src/app.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Archivos estáticos
+app.use(express.static(path.join(__dirname, '../../Front/public')));
+
+// EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../../Front/views'));
+
+// Middleware global
+app.use((req, res, next) => {
+    res.locals.activePage = '';
+    next();
+});
+
+// Routes
+const forumRoutes = require('./presentation/routes/forum/getForum.routes');
+app.use('/', forumRoutes);
+
+module.exports = app;
