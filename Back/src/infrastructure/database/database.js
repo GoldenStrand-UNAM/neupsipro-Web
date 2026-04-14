1de99323896e45da91a79ce8b1933abf5fb4c5fd
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-04-13 23:54:00 -0600

    fix(userList): database

--- Back/src/infrastructure/database/database.js
+++ Back/src/infrastructure/database/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root', 
-    password: '123456', 
+    password: '****', 
     database: 'unam01eng', 
     port: 3306,
 });
