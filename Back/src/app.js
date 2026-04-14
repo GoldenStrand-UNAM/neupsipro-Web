dc2883fe12ba1cbd56d82ada3def946c16755372
Jh700wolf <jh700wolf@gmail.com>
2026-04-14 17:03:06 -0600

    build(back) Adds the basic configuration needed in all branches and newenv example.

---
+++ Back/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require("path");
+const cors = require("cors");
+
+
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '..', '..', 'Front', 'views'));
+app.use(express.static(path.join(__dirname, '..', '..', 'Front', 'public')));
+app.use(cors());
+app.use(express.json());
+
+
+
+
+module.exports = app;
+
