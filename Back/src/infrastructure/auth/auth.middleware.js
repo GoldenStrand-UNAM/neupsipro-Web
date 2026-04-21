7d6f62a5ca5b1019e5109c257c8cdd1d003c0383
Frida Arcadia <frida.arcadialv@gmail.com>
2026-04-21 11:51:08 -0600

    fix(login): fix eslint warnings

--- Back/src/infrastructure/auth/auth.middleware.js
+++ Back/src/infrastructure/auth/auth.middleware.js
@@ -7,20 +7,17 @@ class AuthMiddleware {
         const cookies = req.cookies || {};
         const token = cookies.jwt_token;
         if (!token) {
-            console.log("Middleware: No hay token");
             return res.redirect('/auth/');
         }
         try {
             if (this.authService && this.authService.isBlacklisted(token)) {
-                console.log("Token en blacklist. Sesión cerrada.");
                 res.clearCookie('jwt_token');
                 return res.redirect('/auth/');
             }
             const decoded = this.jwtService.verifyToken(token);
             req.user = {userId: decoded.userId};
             next();
-        } catch (error) {
-            console.error("Token invalido o expirado:", error.message);
+        } catch {
             res.clearCookie('jwt_token');
             return res.redirect('/auth/');
         }
