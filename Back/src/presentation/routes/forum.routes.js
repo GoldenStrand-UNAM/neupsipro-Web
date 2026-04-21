7ab7316cff3911449371f48ab962143076863173
Frida Arcadia <frida.arcadialv@gmail.com>
2026-04-21 00:45:06 -0600

    fix(login): fix linter errors/warnings

--- Back/src/presentation/routes/forum.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const forumController = require("../controller/forum.controller")
-
-module.exports = () => {
-    const router = express.Router();
-    router.get("/:idPublication", forumController.consultPublication);
-    router.get("/", forumController.consultPublications);
-    return router;
-};
