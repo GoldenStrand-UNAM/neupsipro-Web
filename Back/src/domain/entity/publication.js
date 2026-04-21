eb3e1ecf4361e326b62ff9e72affdc7857e37f9c
Frida Arcadia <frida.arcadialv@gmail.com>
2026-04-21 17:40:28 -0600

    fix(changelog): change changelog language

---
+++ Back/src/domain/entity/publication.js
@@ -0,0 +1,28 @@
+
+class Publication {
+  constructor ({ id_publicacion, id_usuario, titulo, contenido, imagenes, fecha_y_hora }) {
+    this._validate(titulo);
+
+    this.id_publicacion = id_publicacion || null;
+    this.id_usuario = id_usuario;
+    this.titulo = titulo.trim();
+    this.contenido = contenido || null;
+    this.imagenes = imagenes || null; // string with URL of image, S3
+    this.fecha_y_hora = fecha_y_hora || null;
+  }
+
+  _validate (titulo) {
+    if (!titulo || titulo.trim().length === 0) {
+      throw new Error('El título es obligatorio');
+    }
+    if (titulo.trim().length > 100) {
+      throw new Error('El título no puede superar 100 caracteres');
+    }
+  }
+
+  setId (id) {
+    this.id_publicacion = id;
+  }
+}
+
+module.exports = Publication;
