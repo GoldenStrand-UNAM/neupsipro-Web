ec75b53df3539e5f3162bd08f146ec51d13b3af0
Andrey <enriqueandrey@hotmail.com>
2026-04-22 00:32:45 -0600

    feat(forum): added auth and acl implementation into routes

--- Back/src/presentation/routes/forum/getForum.Routes.js
+++ Back/src/presentation/routes/forum/getForum.Routes.js
@@ -1,19 +1,29 @@
-const express = require("express")
-const router = express.Router()
-
-
+const express = require("express");
 
 const ForumRepository = require("../../../infrastructure/repositories/forumRepository");
 const ForumController = require("../../controller/forum/GetForum.Controller");
 const GetForumUseCase = require("../../../application/usecase/forum/getForumUseCase");
+const JwtService = require("../../../infrastructure/external/jwt.service");
+const AuthMiddleware = require("../../../infrastructure/auth/auth.middleware");
+const PermissionsMiddleware = require("../../../infrastructure/auth/permissions.middleware");
 
-const repository = new ForumRepository();
-const useCase = new GetForumUseCase(repository);
-const controller = new ForumController(useCase);
+module.exports = (authUseCase) => {
+    const router = express.Router();
 
-router.get('/', (req, res) => controller.getForum(req, res));
+    const repository = new ForumRepository();
+    const useCase = new GetForumUseCase(repository);
+    const controller = new ForumController(useCase);
 
+    const jwtService = new JwtService();
+    const authMiddleware = new AuthMiddleware(jwtService);
+    const permissionsMiddleware = new PermissionsMiddleware(authUseCase);
 
+    router.get(
+        '/',
+        authMiddleware.verifyToken,
+        permissionsMiddleware.requirePermission('Forum', 'consultation'),
+        (req, res) => controller.getForum(req, res)
+    );
 
-
-module.exports = router
+    return router;
+};
