5a276b71619a337f3ae9155d1a089db26d425545
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-04-25 11:39:27 -0600

    feat(consullt-clinical): added userClinicalDTO

---
+++ Back/src/application/dto/userClinicalDTO.js
@@ -0,0 +1,7 @@
+class UserClinicalSummaryDTO {
+    constructor(userSummary) {
+        this.id = userSummary.id;
+        this.fullName = userSummary.fullName;
+    }
+}
+module.exports = UserClinicalSummaryDTO;
