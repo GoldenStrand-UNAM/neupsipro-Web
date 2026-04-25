b10173f289d7004b5d99bdaeccff143e25bf7cfa
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-04-25 13:11:45 -0600

    feat(consullt-clinical): added clinical user consult the ACL functionality

--- Back/src/presentation/routes/clinical/getUsersListClinical.routes.js
+++ Back/src/presentation/routes/clinical/getUsersListClinical.routes.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const router = express.Router();
+
 const ClinicalRepository = require("../../../infrastructure/repositories/clinicalRepository");
 const getClinicalListUseCase = require("../../../application/usecase/clinical/getClinicalListUseCase");
 const getUsersClinicalListController = require("../../controller/clinical/getUsersListClinical.controller");
 
-const repository = new ClinicalRepository();
-const useCase = new getClinicalListUseCase(repository);
-const controller = new getUsersClinicalListController(useCase);
+const JwtService = require("../../../infrastructure/external/jwt.service");
+const AuthMiddleware = require("../../../infrastructure/auth/auth.middleware");
+const PermissionsMiddleware = require("../../../infrastructure/auth/permissions.middleware");
 
-router.get("/clinico", (req, res) => controller.getUsersPage(req, res));
-router.get("/api/usuarios-clinicos", (req, res) => controller.getUsers(req, res));
-module.exports = router;
+module.exports = (authUseCase) => {
+    const router = express.Router();
+
+    const repository = new ClinicalRepository();
+    const useCase = new getClinicalListUseCase(repository);
+    const controller = new getUsersClinicalListController(useCase);
+
+    const jwtService = new JwtService();
+    const authMiddleware = new AuthMiddleware(jwtService);
+    const permissionsMiddleware = new PermissionsMiddleware(authUseCase);
+
+    router.get("/clinico",  authMiddleware.verifyToken, permissionsMiddleware.requirePermission('clinical', 'consultation'), (req, res) => controller.getUsersPage(req, res));
+    router.get("/api/usuarios-clinicos", authMiddleware.verifyToken, permissionsMiddleware.requirePermission('clinical', 'consultation'),  (req, res) => controller.getUsers(req, res));
+
+
+
+    return router;
+};
