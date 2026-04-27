d8c1a116c3befaa6623b76260c28ff893ad6a463
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-04-26 21:47:33 -0600

    fix(postPublication): fixed the middlewares

--- Back/src/presentation/routes/forum/postPublication.routes.js
+++ Back/src/presentation/routes/forum/postPublication.routes.js
@@ -1,23 +1,42 @@
 const express = require('express');
-const ForumRepository = require('../../../infrastructure/repositories/ForumRepository');
+const ForumRepository = require('../../../infrastructure/repositories/forumRepository');
 const PostPublicationController = require('../../controller/forum/postPublication.Controller');
 const RegPublicationUseCase = require('../../../application/usecase/forum/postPublicationUseCase');
 const upload = require('../../../infrastructure/external/multer.service');
 const s3UploadMiddleware = require('../../../infrastructure/external/s3.middleware');
 const validateImageMiddleware = require('../../../infrastructure/external/validateImage.middleware');
 
+const JwtService = require('../../../infrastructure/external/jwt.service');
+const AuthMiddleware = require('../../../infrastructure/auth/auth.middleware');
+const PermissionsMiddleware = require('../../../infrastructure/auth/permissions.middleware');
 
+module.exports = (authUseCase) => {
+    const router = express.Router();
 
-const router = express.Router();
+    const repository = new ForumRepository();
+    const useCase = new RegPublicationUseCase(repository);
+    const controller = new PostPublicationController(useCase);
 
-const repository = new ForumRepository();
-const useCase = new RegPublicationUseCase(repository);
-const controller = new PostPublicationController(useCase);
+    const jwtService = new JwtService();
+    const authMiddleware = new AuthMiddleware(jwtService);
+    const permissionsMiddleware = new PermissionsMiddleware(authUseCase);
 
-router.get('/forum/registrar', (req, res) => {
-    res.render('Forum/postPublication', { activePage: 'foro' });
-});
+    router.get(
+        '/forum/registrar',
+        //authMiddleware.verifyToken,
+       // permissionsMiddleware.requirePermission('Forum', 'writing'),
+        (req, res) => res.render('forum/postPublication', { activePage: 'foro' })
+    );
 
-router.post('/forum/registrar', upload.single('imagen'), validateImageMiddleware, s3UploadMiddleware,  (req, res) => controller.registerPublication(req, res));
+    router.post(
+        '/forum/registrar',
+        //authMiddleware.verifyToken,                                     
+        //permissionsMiddleware.requirePermission('Forum', 'writing'),
+        upload.single('imagen'),
+        validateImageMiddleware,
+        s3UploadMiddleware,
+        (req, res) => controller.registerPublication(req, res)
+    );
 
-module.exports = router;
+    return router; 
+};
