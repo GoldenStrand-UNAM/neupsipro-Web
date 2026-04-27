880a5592bd71a2b659eb9bf479da443be79c98d0
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-04-26 19:57:32 -0600

    feat(postPublication): added the entity and impoRepository of postPublication

--- Back/src/domain/repository/ImpForumRepository.js
+++ Back/src/domain/repository/ImpForumRepository.js
@@ -3,8 +3,12 @@
 class ImpForumRepository {
 
     // Method to retrieve paginated forum data (must be implemented)
-    async fetchAll () {
+    async fetchAll ({_page,_limit}) {
         throw new Error("fetchAll() must be implemented");
+    }
+    // Method to save a new publication 
+    async save ({ _id_usuario, _titulo, _contenido, _imagenes }) {
+        throw new Error('save() must be implemented');
     }
 }
 
