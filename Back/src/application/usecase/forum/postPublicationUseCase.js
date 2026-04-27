4e5cfec5c59e6aade5a2ea88ad8da1515c26120b
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-04-26 19:56:02 -0600

    feat(postPublication): added the application of post publication

---
+++ Back/src/application/usecase/forum/postPublicationUseCase.js
@@ -0,0 +1,26 @@
+// Forum usecase  validates and registers a new publication
+const Publication = require('../../../domain/entity/Publication');
+const PublicationDTO = require('../../dto/PublicationDTO');
+
+class RegPublicationUseCase {
+  constructor (ForumRepository) {
+    this.ForumRepository = ForumRepository;
+  }
+
+  async execute ({ id_usuario, titulo, contenido, image }) {
+    // Entity validation 
+    const publication = new Publication ({
+      id_usuario,
+      titulo,
+      contenido,
+      image: image || null, // S3 Link or null
+    });
+
+    const saved = await this.ForumRepository.save(publication);
+
+    // Map saved into clean DTO for the client
+    return PublicationDTO.fromEntity(saved);
+  }
+}
+
+module.exports = RegPublicationUseCase;
