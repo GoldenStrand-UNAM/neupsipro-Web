77d0cf02c48be085f497d5126a0fbd241007df4b
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-04-26 20:03:21 -0600

    feat(postPublication): added the route

---
+++ Back/src/presentation/routes/forum/postPublication.routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const ForumRepository = require('../../../infrastructure/repositories/ForumRepository');
+const PostPublicationController = require('../../controller/forum/postPublication.Controller');
+const RegPublicationUseCase = require('../../../application/usecase/forum/postPublicationUseCase');
+const upload = require('../../../infrastructure/external/multer.service');
+const s3UploadMiddleware = require('../../../infrastructure/external/s3.middleware');
+const validateImageMiddleware = require('../../../infrastructure/external/validateImage.middleware');
+const { publicationLimiter }  = require('../../../infrastructure/external/rateLimiter');
+
+
+
+const router = express.Router();
+
+const repository = new ForumRepository();
+const useCase = new RegPublicationUseCase(repository);
+const controller = new PostPublicationController(useCase);
+
+router.get('/forum/registrar', (req, res) => {
+    res.render('Forum/postPublication', { activePage: 'foro' });
+});
+
+router.post('/forum/registrar', publicationLimiter, upload.single('imagen'), validateImageMiddleware, s3UploadMiddleware,  (req, res) => controller.registerPublication(req, res));
+
+module.exports = router;
