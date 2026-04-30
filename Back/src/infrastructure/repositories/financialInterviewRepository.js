a8c08c2b3f6eed738f976516a993376ed7f139d9
Nicole Uriostegui <a01711853@tec.mx>
2026-04-30 15:47:28 -0600

    feat(financial_interview): add entity and fixes for business logic

--- Back/src/infrastructure/repositories/financialInterviewRepository.js
+++ Back/src/infrastructure/repositories/financialInterviewRepository.js
@@ -1,5 +1,6 @@
 const db = require ("../database/database");
 const ImpFinancialInterviewRepository = require('../../domain/repository/ImpFinancialInterviewRepository');
+const FinancialInterview = require("../../domain/entity/FinancialInterview");
 
 class FinancialInterviewRepository extends ImpFinancialInterviewRepository {
 
@@ -25,57 +26,74 @@ class FinancialInterviewRepository extends ImpFinancialInterviewRepository {
 
     // Fetch financial situation by relation
     async fetchFinancialSituation ({ id_user_relation }) {
-        return await db.query(
-            `SELECT fs.*, cp.*
-             FROM financial_situation AS fs,
-                  contributing_people AS cp
-             WHERE id_user_relation = ?`,
+        const rows = await db.query(
+            `SELECT fs.*, cp.contributor, cp.relation, cp.income
+            FROM financial_situation fs
+            LEFT JOIN contributing_people cp
+                ON cp.id_user_relation = fs.id_user_relation
+            WHERE fs.id_user_relation = ?`,
             [id_user_relation]
         );
+
+        return new FinancialInterview({
+            data: rows,
+            current_section: 1,
+        });
     }
 
     // Fetch ESC Goverment by relation
     async fetchEscGov ({ id_user_relation }) {
-        return await db.query(
+        const rows =  await db.query(
             `SELECT *
              FROM esc_government
              WHERE id_user_relation = ?`,
             [id_user_relation]
         );
+
+        return rows.map(row => new FinancialInterview({
+            data: row,
+            current_section: 2,
+        }));
     }
 
     // Fetch AMAI Wuestionary by relation
     async fetchAmaiQ ({ id_user_relation }) {
-        return await db.query(
+        const rows =  await db.query(
             `SELECT *
              FROM amai_questionnaire
              WHERE id_user_relation = ?`,
             [id_user_relation]
         );
+
+        return rows.map(row => new FinancialInterview({
+            data: row,
+            current_section: 3,
+        }));
     }
 
     // Fetch results by relation
     async fetchResults ({ id_user_relation }) {
-        return await db.query(
+        const rows = await db.query(
             `SELECT 
                 fs.total_income, 
                 fs.total_expenses,
-
                 g.socioeconomic_level AS socio_level_gov, 
                 g.total AS total_gov,
-
                 a.socioeconomic_level AS socio_level_amai, 
                 a.total AS total_amai
-
-             FROM financial_situation AS fs
-             INNER JOIN esc_government AS g 
+            FROM financial_situation AS fs
+            INNER JOIN esc_government AS g 
                 ON g.id_user_relation = fs.id_user_relation
-             INNER JOIN amai_questionnaire AS a 
+            INNER JOIN amai_questionnaire AS a 
                 ON a.id_user_relation = fs.id_user_relation
-
-             WHERE fs.id_user_relation = ?`,
+            WHERE fs.id_user_relation = ?`,
             [id_user_relation]
         );
+
+        return rows.map(row => new FinancialInterview({
+            data: row,
+            current_section: 4,
+        }));
     }
 }
 
