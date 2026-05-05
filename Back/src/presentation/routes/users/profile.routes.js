d102db2231b35d69284401741c813495ab41607f
nopusmiau <ricardoandraderobledo@gmail.com>
2026-05-05 06:51:11 -0600

    fix(consultProfile): fixed new lint errors by changes done

--- Back/src/presentation/routes/users/profile.routes.js
+++ Back/src/presentation/routes/users/profile.routes.js
@@ -3,22 +3,22 @@ const ImpProfileRepository = require('../../../infrastructure/repositories/ImpPr
 const GetProfileUseCase = require('../../../application/usecase/users/getProfileUseCase');
 const ProfileController = require('../../controller/users/profile.controller');
 const JwtService = require('../../../infrastructure/external/jwt.service');
-const AuthMiddleware = require('../../../infrastructure/auth/auth.middleware')
+const AuthMiddleware = require('../../../infrastructure/auth/auth.middleware');
 const db = require('../../../infrastructure/database/database');
 
 module.exports = (_authUseCase) => {
-    const router = express.Router();
-    const impProfileRepository = new ImpProfileRepository(db);
-    const getProfileUseCase = new GetProfileUseCase(impProfileRepository);
-    const profileController = new ProfileController(getProfileUseCase);
-    const jwtService = new JwtService();
-    const authMiddleware = new AuthMiddleware(jwtService);
+  const router = express.Router();
+  const impProfileRepository = new ImpProfileRepository(db);
+  const getProfileUseCase = new GetProfileUseCase(impProfileRepository);
+  const profileController = new ProfileController(getProfileUseCase);
+  const jwtService = new JwtService();
+  const authMiddleware = new AuthMiddleware(jwtService);
 
-router.get(
+  router.get(
     '/:userId',
     authMiddleware.verifyToken,
     (req, res) => profileController.getProfile(req, res)
-);
+  );
 
-return router;
+  return router;
 };
