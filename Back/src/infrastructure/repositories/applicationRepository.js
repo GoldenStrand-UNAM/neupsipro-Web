b368a228d7fb624c3892534fd743525965185b36
Andrey <enriqueandrey@hotmail.com>
2026-05-04 23:18:54 -0600

    feat(tests): add new function to test application entity for insert a new session

--- Back/src/infrastructure/repositories/applicationRepository.js
+++ Back/src/infrastructure/repositories/applicationRepository.js
@@ -1,16 +1,16 @@
 const db = require ('../database/database');
-const TestSessions = require('../../domain/entity/testSessions');
+const TestSessions = require('../../domain/entity/testApplication');
 
-class TestSessionsRepository {
+class TestApplicationsRepository {
   async fetchTestSessions ({ id_user }) {
     const [testSessions] = await db.query(
       `SELECT *
-      FROM test_sessions ts
-      WHERE ts.id_user = ?`,
+      FROM test_applications ta
+      WHERE ta.id_user = ?`,
       [id_user]
     );
     return testSessions.map(row => new TestSessions(row));
   }
 }
 
-module.exports = TestSessionsRepository;
+module.exports = TestApplicationsRepository;
