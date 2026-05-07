f805d9eae818e7f2f00363c7b11d50db55888bef
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-05-06 19:20:26 -0600

    fix(clinicList): added the new information to be shown

--- Back/src/application/dto/userClinicalSummaryDTO.js
+++ Back/src/application/dto/userClinicalSummaryDTO.js
@@ -2,6 +2,9 @@ class UserClinicalSummaryDTO {
   constructor (userSummary) {
     this.id = userSummary.id;
     this.fullName = userSummary.fullName;
+    this.affiliation = userSummary.affiliation;
+    this.activity = userSummary.activity;
+    this.assignedCount = userSummary.assignedCount;
   }
 }
 module.exports = UserClinicalSummaryDTO;
