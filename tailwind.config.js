0793782414442490f7cc3181467c5fb157a48a2e
Andrey <enriqueandrey@hotmail.com>
2026-05-07 23:55:20 -0600

    feat(tests): deleted tailwind colors status

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,43 +3,9 @@ module.exports = {
   content: ["./Front/src/**/*.{html,js}", "./Front/public/views/**/*.html", "./Front/views/**/*.ejs", "./front/Public/js/**/*.js"],
   theme: {
     extend: {
-
-      fontFamily: {
+      fontFamily:{
         sans: ['Roboto'],
       },
-
-      colors: {
-
-        brand: {
-          primary: '#3350A9',
-        },
-
-        status: {
-
-          neutral: {
-            bg: '#F3F4F6',
-            badge: '#6B7280',
-          },
-
-          warning: {
-            bg: '#FEF3C7',
-            badge: '#F59E0B',
-          },
-
-          success: {
-            bg: '#DCFCE7',
-            badge: '#22C55E',
-          },
-
-          fatal: {
-            bg: '#FEE2E2',
-            badge: '#EF4444',
-          },
-
-        },
-
-      },
-
     },
   },
   plugins: [],
