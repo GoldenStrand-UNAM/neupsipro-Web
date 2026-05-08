068a9615efc6a23239c1e93e4a3567de59d55eb9
Andrey <enriqueandrey@hotmail.com>
2026-05-08 00:32:00 -0600

    feat(tests): add new methods into result repository

--- Back/src/domain/repository/resultRepository.js
+++ Back/src/domain/repository/resultRepository.js
@@ -1,7 +1,18 @@
 // Interface contract for test result persistence.
 class resultRepository {
+
+  //Fetch all test results for a user
+  async fetchUserTests({ id_user }) {
+    throw new Error('fetchUserTests() not implemented');
+  }
+
   async createResults (_id_application, _id_user, _tests) {
     throw new Error('createResults() must be implemented');
+  }
+
+  //Fetch all test results scoped to a specific application
+  async fetchTestsByApplication({ id_user, id_application }) {
+    throw new Error('fetchTestsByApplication() not implemented');
   }
 }
 
