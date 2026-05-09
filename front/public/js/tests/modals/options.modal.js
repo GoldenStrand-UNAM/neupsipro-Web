13c1247706a9ceb4b796319e0d4c759ac685d141
AndreyLira <enriqueandrey@hotmail.com>
2026-05-09 13:38:47 -0600

    feat(tests): change colors

--- front/public/js/tests/modals/options.modal.js
+++ front/public/js/tests/modals/options.modal.js
@@ -27,7 +27,7 @@ function openOptionsModal(idUser, idApplication, test) {
           class="w-full flex items-center gap-4 px-6 py-5 rounded-2xl border
                  border-gray-200 hover:border-[#3350A9] hover:bg-[#3350A9]/5
                  transition cursor-pointer text-left">
-          <svg class="w-6 h-6 text-[#3350A9] shrink-0" xmlns="http://www.w3.org/2000/svg"
+          <svg class="w-6 h-6 text-[#000000] shrink-0" xmlns="http://www.w3.org/2000/svg"
                fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15"/>
           </svg>
@@ -43,7 +43,7 @@ function openOptionsModal(idUser, idApplication, test) {
                  border-gray-200 transition text-left
                  ${hasScore ? 'hover:border-[#3350A9] hover:bg-[#3350A9]/5 cursor-pointer' : 'opacity-40 cursor-not-allowed'}"
           ${hasScore ? '' : 'disabled'}>
-          <svg class="w-6 h-6 text-[#3350A9] shrink-0" xmlns="http://www.w3.org/2000/svg"
+          <svg class="w-6 h-6 text-[#000000] shrink-0" xmlns="http://www.w3.org/2000/svg"
                fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round"
                   d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582
@@ -64,7 +64,7 @@ function openOptionsModal(idUser, idApplication, test) {
                  border-gray-200 transition text-left
                  ${hasScore ? 'hover:border-[#3350A9] hover:bg-[#3350A9]/5 cursor-pointer' : 'opacity-40 cursor-not-allowed'}"
           ${hasScore ? '' : 'disabled'}>
-          <svg class="w-6 h-6 text-[#3350A9] shrink-0" xmlns="http://www.w3.org/2000/svg"
+          <svg class="w-6 h-6 text-[#000000] shrink-0" xmlns="http://www.w3.org/2000/svg"
                fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round"
                   d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12
