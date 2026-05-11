8b450bc31dc78488fb6199dc906f4af36ccba57f
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-05-11 03:44:29 -0600

    fix(creatAppointment): error changed to spanish

--- Back/src/application/usecase/appointments/createAppointmentUseCase.js
+++ Back/src/application/usecase/appointments/createAppointmentUseCase.js
@@ -6,21 +6,18 @@ class createAppointmentUseCase {
   async execute ({ id_user, id_clinic_user, issue, date_time }) {
     //validations
     if (!id_user || !id_clinic_user || !issue || !date_time) {
-      throw new Error('All fields are required');
+      throw new Error('Todos los campos son requeridos');
     }
 
     const appointmentDate = new Date(date_time);
     if (isNaN(appointmentDate.getTime())) {
-      throw new Error('Invalid date');
-    }
-    if (appointmentDate <= new Date()) {
-      throw new Error('The appointment must be scheduled for a future date');
+      throw new Error('Fecha inválida');
     }
 
     // validate it doesn´t exists a future appointment
     const upcoming = await this.appointmentRepository.findUpcomingByUser({ id_user });
     if (upcoming) {
-      throw new Error('This user already has a future appointment scheduled');
+      throw new Error('Este usuario ya tiene una cita programada para el futuro');
     }
     // creates the relation for the appointment type
     const id_user_relation = await this.appointmentRepository.findOrCreateUserRelation({
