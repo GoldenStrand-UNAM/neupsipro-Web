23dfb59bd9ec1f6198a5a4d8f872ed0505f5219f
AndreyLira <enriqueandrey@hotmail.com>
2026-05-10 19:22:03 -0600

    feat(tests): controller for REY

---
+++ Back/src/presentation/controller/testApplications/postREY.controller.js
@@ -0,0 +1,20 @@
+class postREYController {
+  constructor(useCase) {
+    this.useCase = useCase;
+  }
+
+  async postResult(req, res) {
+    const { id_user, id_application } = req.params;
+    const { score, notes }            = req.body;
+
+    try {
+      const data = await this.useCase.execute({ id_user, id_application, score, notes });
+      return res.status(200).json({ data });
+    } catch (err) {
+      const httpStatus = err.status || 500;
+      return res.status(httpStatus).json({ error: err.message });
+    }
+  }
+}
+
+module.exports = postREYController;
