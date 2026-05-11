09495fce87d37e64cb042e645eba8d5cc836abb2
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-05-11 02:44:13 -0600

    feat(creatAppointment): appointment reppositories that are needed

---
+++ Back/src/domain/repository/appointmentRepository.js
@@ -0,0 +1,12 @@
+class appointmentRepository {
+  async findUpcomingByUser ({ _id_user }) {
+    throw new Error('findUpcomingByUser() must be implemented');
+  }
+  async createAppointment ({ _id_user_relation, _issue, _date_time }) {
+    throw new Error('createAppointment() must be implemented');
+  }
+  async findOrCreateUserRelation ({ _id_user, _id_clinic_user }) {
+    throw new Error('findOrCreateUserRelation() must be implemented');
+  }
+}
+module.exports = appointmentRepository;
