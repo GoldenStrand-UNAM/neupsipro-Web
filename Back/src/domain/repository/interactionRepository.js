82a922ba8daa12368c442b25d826bbe58d4188ee
Jh700wolf <jh700wolf@gmail.com>
2026-05-10 20:26:40 -0600

    feat(consult-publication) Adds complete feature of consult publication

---
+++ Back/src/domain/repository/interactionRepository.js
@@ -0,0 +1,16 @@
+class interactionRepository {
+  // Method to retrive one publication with its interactions.
+  async fetchAllFromOne () {
+    throw new Error('fetchAllFromOne() interaction is not working.');
+  }
+  // Method to retrive the number of likes.
+  async fetchLikes () {
+    throw new Error('fetchLikes() interacrion is not working.');
+  }
+  // Method to retrive the number of comments.
+  async fetchComments () {
+    throw new Error('fetchComments() interacrion is not working.');
+  }
+
+}
+module.exports = interactionRepository;
