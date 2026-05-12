a7ccce62a9722d7e4114c73f66dcede85ce2e9dc
Frida Arcadia <frida.arcadialv@gmail.com>
2026-05-12 13:09:51 -0600

    feat(consultClinical): finish rendering patients table

--- Back/src/application/usecase/clinical/getClinicalUserUseCase.js
+++ Back/src/application/usecase/clinical/getClinicalUserUseCase.js
@@ -1,5 +1,4 @@
 const ClinicalUserDTO = require('../../dto/clinicalDTO');
-const UserDTO = require('../../dto/userDTO');
 
 class consultClinicalUserUseCase {
   constructor (clinicalRepository) {
@@ -9,21 +8,17 @@ class consultClinicalUserUseCase {
   async execute ({ id_user }) {
 
     const clinicalUser = await this.clinicalRepository.fetchClinical({ id_user });
-    const patientsPerClinical = await this.clinicalRepository.fetchPatientsAssigned({ id_user });
 
     if (!clinicalUser || clinicalUser.length === 0) {
       throw new Error('Usuario no encontrado');
     }
 
     const user = clinicalUser[0];
-    const patients = patientsPerClinical;
 
     const cleanUser = ClinicalUserDTO.fromEntity(user);
-    const cleanPatients = patients.map((patient) => UserDTO.fromEntity(patient));
 
     return {
       ...cleanUser,
-      patients: cleanPatients,
     };
   }
 }
--- Back/src/presentation/routes/clinical/getClinicalUser.routes.js
+++ Back/src/presentation/routes/clinical/getClinicalUser.routes.js
@@ -9,27 +9,24 @@ const AuthMiddleware = require('../../../infrastructure/auth/auth.middleware');
 const PermissionsMiddleware = require('../../../infrastructure/auth/permissions.middleware');
 
 const GetClinicalUserUseCase = require('../../../application/usecase/clinical/getClinicalUserUseCase');
+const GetClinicalPatientsUseCase = require('../../../application/usecase/clinical/getClinicalPatientsUseCase');
 
 module.exports = (authUseCase) => {
 
   const clinicalUserRepository    = new ImpClinicalRepository();
   const useCase = new GetClinicalUserUseCase(clinicalUserRepository);
-  const controller = new ClinicalUserController(useCase);
+  const secondUseCase = new GetClinicalPatientsUseCase(clinicalUserRepository);
+  const controller = new ClinicalUserController(useCase, secondUseCase);
   const jwtService = new JwtService();
   const authMiddleware = new AuthMiddleware(jwtService);
   const permissionsMiddleware = new PermissionsMiddleware(authUseCase);
+
+  router.get('/clinical-patient', authMiddleware.verifyToken, permissionsMiddleware.requirePermission('clinical', 'consultation'),  (req, res) => controller.getPatients(req, res));
 
   router.get(
     '/:id_user', authMiddleware.verifyToken,
     permissionsMiddleware.requirePermission('user management', 'consultation'), (req, res) => controller.getClinicalUser(req, res)
   );
-
-  router.get('/consultUser', (req, res) => {
-    res.render('clinical/consultUser', {
-      activePage: 'usuario',
-
-    });
-  });
 
   return router;
 };
