f43c6a0cdae5384c3ee00a372f6add85924a6165
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-05-12 18:09:16 -0600

    feat(intervention): new atributes to show

--- Back/src/domain/entity/intervention.js
+++ Back/src/domain/entity/intervention.js
@@ -1,9 +1,14 @@
 class Intervention {
   constructor (data) {
-    this.idIntervention = data.id_intervention;
-    this.idUser = data.id_user;
-    this.neuropsychProfile = data.neuropsych_profile;
-    this.contractLink = data.contract_link;
+    this.idIntervention   = data.id_intervention;
+    this.idUser           = data.id_user;
+    this.contractLink     = data.contract_link;
+    this.createdAt        = data.created_at;
+    this.userFullName     = data.user_full_name || null;
+    this.referenceNumber  = data.reference_number || null;
+    this.photo            = data.profile_photo || null;
+    this.schooling        = data.schooling || null;
+    this.ocupation        = data.ocupation || null;
   }
 }
 module.exports = Intervention;
