56ca873a2c121c680cc18901b91a6f510f874bac
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-05-12 18:13:25 -0600

    feat(intervention): intervention repository new features

--- Back/src/domain/repository/interventionRepository.js
+++ Back/src/domain/repository/interventionRepository.js
@@ -2,11 +2,20 @@ class interventionRepository {
   async findByUser ({ _id_user }) {
     throw new Error('findByUser() must be implemented');
   }
-  async createIntervention ({ _id_user, _neuropsych_profile, _contract_link }) {
+  async createIntervention ({ _id_user, _contract_link }) {
     throw new Error('createIntervention() must be implemented');
   }
-  async deleteByUser ({ _id_user }) {
-    throw new Error('deleteByUser() must be implemented');
+  async findSessionsByIntervention ({ _id_intervention }) {
+    throw new Error('findSessionsByIntervention() must be implemented');
+  }
+  async findLastSession ({ _id_intervention }) {
+    throw new Error('findLastSession() must be implemented');
+  }
+  async createSession ({ _id_intervention, _session_number, _session_date, _objectives, _development, _dqp_task }) {
+    throw new Error('createSession() must be implemented');
+  }
+  async deleteSession ({ _id_session }) {
+    throw new Error('deleteSession() must be implemented');
   }
 }
 module.exports = interventionRepository;
