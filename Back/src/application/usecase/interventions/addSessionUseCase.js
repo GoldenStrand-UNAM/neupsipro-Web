cf07effdee7f7d04edaae86a53d099638e5b8b28
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-05-13 00:54:05 -0600

    feat(intervention): usecase to add session, delete session get intervention and uptade contract link and neuro profil

---
+++ Back/src/application/usecase/interventions/addSessionUseCase.js
@@ -0,0 +1,25 @@
+class addSessionUseCase {
+  constructor (interventionRepository) {
+    this.interventionRepository = interventionRepository;
+  }
+
+  async execute ({ id_user, session_number, session_date, objectives, development, dqp_task }) {
+    if (!id_user) throw new Error('id_user es requerido');
+    if (!session_date) throw new Error('La fecha de la sesión es requerida');
+
+    const intervention = await this.interventionRepository.findByUser({ id_user });
+    if (!intervention) throw new Error('No existe intervención activa');
+
+    const idSession = await this.interventionRepository.createSession({
+      id_intervention: intervention.idIntervention,
+      session_number:  session_number || null,
+      session_date,
+      objectives: objectives || null,
+      development: development || null,
+      dqp_task: dqp_task || null,
+    });
+
+    return { success: true, idSession, message: 'Sesión agregada' };
+  }
+}
+module.exports = addSessionUseCase;
