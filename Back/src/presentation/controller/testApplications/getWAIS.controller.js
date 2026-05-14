d1ca8224daa87b1ca1076873ead6eb47f4034844
AndreyLira <enriqueandrey@hotmail.com>
2026-05-13 18:47:15 -0600

    feat(WAIS): wais controller for getting information

---
+++ Back/src/presentation/controller/testApplications/getWAIS.controller.js
@@ -0,0 +1,25 @@
+class getWAISController {
+
+  constructor (getWAISResultUseCase) {
+    this.useCase = getWAISResultUseCase;
+  }
+
+  async getResult (req, res) {
+    try {
+      const { id_results } = req.params;
+
+      const dto = await this.useCase.execute({ id_results });
+
+      return res.status(200).json({ data: dto });
+
+    } catch (err) {
+      if (err.status && err.message) {
+        return res.status(err.status).json({ error: err.message });
+      }
+      console.error('[getWAISController]', err);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+}
+
+module.exports = getWAISController;
