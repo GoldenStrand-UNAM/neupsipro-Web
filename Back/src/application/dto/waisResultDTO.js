ebc8a7c6b49f0cd25016a2c443d8aaa21ed4f00e
AndreyLira <enriqueandrey@hotmail.com>
2026-05-13 18:25:12 -0600

    feat(WAIS): map data for wais results

---
+++ Back/src/application/dto/waisResultDTO.js
@@ -0,0 +1,38 @@
+class WaisResultsDTO {
+  constructor ({
+    idResults,
+    idTest,
+    status,
+    dateApplied,
+    areas,
+    scoreTotal,
+    notes,
+  }) {
+    this.idResults   = idResults;
+    this.idTest      = idTest;
+    this.status      = status;
+    this.dateApplied = dateApplied;
+    this.areas       = {
+      comVerbal: {
+        score:          areas.comVerbal.score,
+        interpretation: areas.comVerbal.interpretation,
+      },
+      razonPerceptual: {
+        score:          areas.razonPerceptual.score,
+        interpretation: areas.razonPerceptual.interpretation,
+      },
+      memWork: {
+        score:          areas.memWork.score,
+        interpretation: areas.memWork.interpretation,
+      },
+      veloProce: {
+        score:          areas.veloProce.score,
+        interpretation: areas.veloProce.interpretation,
+      },
+    };
+    this.scoreTotal  = scoreTotal;
+    this.notes       = notes ?? null;
+  }
+}
+
+module.exports = WaisResultsDTO;
