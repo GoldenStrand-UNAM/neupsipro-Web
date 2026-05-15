860c991700b9867fc343f8cd96d1e710e50f5c87
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-05-14 21:12:27 -0600

    fix(deletePublication): fetch one user forum repo is back, srry choche

--- Back/src/infrastructure/repositories/ImpForumRepository.js
+++ Back/src/infrastructure/repositories/ImpForumRepository.js
@@ -48,6 +48,20 @@ class ImpForumRepository extends forumRepository {
 
     return rows[0];
   }
+    async fetchOne ({ idPublication }) {
+    const result = await db.query(
+      `SELECT 
+                p.id_user,
+                p.time_and_date,
+                p.title,
+                p.content,
+                p.image
+            FROM publication p
+            WHERE p.id_publication = ?`,
+      [idPublication]
+    );
+    return result;
+  }
 
   async fetchOneUser ({ idPublication }) {
     const result = await db.query(
