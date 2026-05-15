ed8985a1a914725267c2d62a3237b1c4ff1368c6
AndreyLira <enriqueandrey@hotmail.com>
2026-05-15 14:24:21 -0600

    fix: side bar active button

--- Back/src/presentation/controller/home/home.controller.js
+++ Back/src/presentation/controller/home/home.controller.js
@@ -2,7 +2,7 @@ class HomeController {
   getHome (req, res) {
     const usuario = req.user;
 
-    return res.render('home.ejs', { user: usuario });
+    return res.render('home.ejs', { user: usuario, activePage: 'home' });
   }
 }
 
