797e56391dd8a6b6a73c62fedad06ebf57543d76
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-05-19 15:23:39 -0600

    feat(intervention): added the limit text length in usecase and front

--- Back/src/application/usecase/interventions/addSessionUseCase.js
+++ Back/src/application/usecase/interventions/addSessionUseCase.js
@@ -7,6 +7,10 @@ class addSessionUseCase {
     if (!id_user) throw new Error('id_user es requerido');
     if (!session_date) throw new Error('La fecha de la sesión es requerida');
 
+    if (development && development.length > 1000) throw new Error('No puede superar los 1000 caracteres');
+    if (objectives  && objectives.length  > 1000) throw new Error('No puede superar los 1000 caracteres');
+    if (dqp_task    && dqp_task.length    > 1000) throw new Error('No puede superar los 1000 caracteres');
+
     const intervention = await this.interventionRepository.findByUser({ id_user });
     if (!intervention) throw new Error('No existe intervención activa');
 
