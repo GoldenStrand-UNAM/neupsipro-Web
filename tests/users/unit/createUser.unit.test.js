b28d278d4814a28c91eb6975e73ae9663b393e79
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-05-20 22:14:37 -0600

    feat(postUser): added the unit  test

--- tests/users/unit/createUser.unit.test.js
+++ tests/users/unit/createUser.unit.test.js
@@ -44,7 +44,7 @@ describe('createUserUseCase',()=>{
         prosthetist:'Carlos',
         neuroEntryDate:'01/01/2024',
         pairs:'Sí asiste',
-        sex:'Never'
+        sex:'Masculino'
     });
 
     test('creates a new user successfully',async()=>{
