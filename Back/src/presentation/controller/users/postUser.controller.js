1f815129a0fb42ad45b8420ecd9b03eb6b9ce85a
Shaula Paz Garay <a01712013tec.mx>
2026-05-22 01:41:43 -0600

    style(createUser): lint and message errors

--- Back/src/presentation/controller/users/postUser.controller.js
+++ Back/src/presentation/controller/users/postUser.controller.js
@@ -36,14 +36,14 @@ class PostUserController {
       return res.status(201).json(user);
     } catch (error) {
       if (error.code === 'ER_DUP_ENTRY' || error.errno === 1062) {
-      return res.status(409).json({ 
-        error: 'Usuario duplicado: ya hay un usuario con ese folio.' 
-      });
-    } else if (error.code !== undefined || error.errno !== undefined) {
-      return res.status(409).json({ 
-        error: 'Error al registrar usuario.' 
-      });
-    }
+        return res.status(409).json({
+          error: 'Usuario duplicado: ya hay un usuario con ese folio o usuario.',
+        });
+      } if (error.code !== undefined || error.errno !== undefined) {
+        return res.status(409).json({
+          error: 'Error al registrar usuario.',
+        });
+      }
       return res.status(400).json({ error: error.message });
     }
   }
