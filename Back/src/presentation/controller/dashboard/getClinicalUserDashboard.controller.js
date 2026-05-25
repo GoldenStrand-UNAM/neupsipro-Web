6c95bac3b87b76622142314a9a05e8d3e9d4d78e
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-05-25 01:30:07 -0600

    feat(tutorial): myDasboard tutorial

--- Back/src/presentation/controller/dashboard/getClinicalUserDashboard.controller.js
+++ Back/src/presentation/controller/dashboard/getClinicalUserDashboard.controller.js
@@ -7,7 +7,7 @@ class GetClinicalUserDashboardController {
   getDashboardView (req, res) {
     try {
       res.locals.activePage = 'dashboard';
-      res.render('dashboard/dashboardClinical');
+      res.render('dashboard/dashboardClinical', { tutorialModule: 'clinicalDashboard' });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
