0c3d12fb8321525b58cc44718b34ff1f105613b0
Shaula Paz Garay <a01712013tec.mx>
2026-05-25 00:54:27 -0600

    feat(create-clinical-user): logic

--- Back/src/presentation/routes/clinical/postClinicalUser.routes.js
+++ Back/src/presentation/routes/clinical/postClinicalUser.routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const {  apiLimiter } = require('../../../infrastructure/external/rateLimiting');
 
 const ImpClinicalRepository = require('../../../infrastructure/repositories/ImpClinicalRepository');
+const PostClinicalUserUseCase = require('../../../application/usecase/clinical/postClinicalUserUseCase');
 const postClinicalUserController = require('../../controller/clinical/postClinicalUser.controller');
 const JwtService = require('../../../infrastructure/external/jwt.service');
 const AuthMiddleware = require('../../../infrastructure/auth/auth.middleware');
 const PermissionsMiddleware = require('../../../infrastructure/auth/permissions.middleware');
 const HashingService = require('../../../infrastructure/external/hashing.service');
+const upload = require('../../../infrastructure/external/multer.service');
 
 module.exports = (authUseCase) => {
 
@@ -14,7 +16,9 @@ module.exports = (authUseCase) => {
 
   const repository = new ImpClinicalRepository();
   const hashingService = new HashingService();
-  const controller = new postClinicalUserController();
+  const useCase = new PostClinicalUserUseCase(repository, hashingService);
+  const controller = new postClinicalUserController(useCase);
+
   const jwtService = new JwtService();
   const authMiddleware = new AuthMiddleware(jwtService);
   const permissionsMiddleware = new PermissionsMiddleware(authUseCase);
@@ -25,6 +29,15 @@ module.exports = (authUseCase) => {
     apiLimiter,
     permissionsMiddleware.requirePermission('user management', 'consultation'),
     (req, res) => controller.postUser(req, res)
+  );
+
+  router.post(
+    '/post',
+    authMiddleware.verifyToken,
+    apiLimiter,
+    permissionsMiddleware.requirePermission('user management', 'writing'),
+    upload.none(),
+    (req, res) => controller.postClinicalUser(req, res)
   );
 
   return router;
