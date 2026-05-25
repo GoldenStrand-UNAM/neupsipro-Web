468baec355979e76ca9805617d7ded2f163f415c
Shaula Paz Garay <a01712013tec.mx>
2026-05-25 02:02:54 -0600

    fix(create-clinical-user): pull errors

--- Back/src/infrastructure/external/validations.js
+++ Back/src/infrastructure/external/validations.js
@@ -31,7 +31,7 @@ class validation {
   validateDate (date, label, required) {
     if (date) {
       const regex = /^\d{2}\/\d{2}\/\d{4}$/;
-      if (!date.match(regex)) throw new Error(`Formato de fecha inválido: ${label}`);
+      if (!date.match(regex)) throw new Error('Formato de fecha inválido');
 
       const [day, month, year] = date.split('/').map(Number);
       const inputDate = new Date(year, month - 1, day);
@@ -41,7 +41,7 @@ class validation {
                           inputDate.getDate() === day;
 
       if (!isRealDate || inputDate >= now || year < 1900) {
-        throw new Error(`${label} debe ser válida, anterior a hoy y después de 1900`);
+        throw new Error('La fecha debe ser válida, anterior a hoy y después de 1900');
       }
 
       //return crypt.encrypt(date);
@@ -50,54 +50,5 @@ class validation {
       throw new Error(`${label} ' debe llenarse`);
     else
       return null;
-  }
-
-  validateFutureDate (date, label, required) {
-    if (date) {
-      const regex = /^\d{2}\/\d{2}\/\d{4}$/;
-      if (!date.match(regex)) throw new Error(`Formato de fecha inválido: ${label}`);
-
-      const [day, month, year] = date.split('/').map(Number);
-      const inputDate = new Date(year, month - 1, day);
-      const now = new Date();
-      now.setHours(0, 0, 0, 0);
-      const isRealDate = inputDate.getFullYear() === year &&
-                          inputDate.getMonth() === month - 1 &&
-                          inputDate.getDate() === day;
-
-      if (!isRealDate || inputDate < now || year > 2100) {
-        throw new Error(`${label} debe ser válida, desde hoy hasta antes de 2100`);
-      }
-
-      //return crypt.encrypt(date);
-      return date;
-    } if (required)
-      throw new Error(`${label} ' debe llenarse`);
-    else
-      return null;
-  }
-
-  validateNumber (param, label, max, required) {
-    const number = Number(param);
-    if (number || number === 0)
-      if (Number.isInteger(number) && number <= max && number >= 0)
-        return number;
-      else
-        throw new Error(`${label} debe ser un entero máximo ${max}`);
-    else if (required)
-      throw new Error(`${label} es obligatorio`);
-    return null;
-  }
-
-  validatePhone (phone, label, required) {
-    this.validate(phone, 15, label, required);
-    if (!required) return phone;
-    const phoneStr = String(phone).trim();
-    const phoneRegex = /^\+?[0-9]+$/;
-
-    if (phoneRegex.test(phoneStr)) {
-      return phoneStr;
-    }
-    throw new Error(`${label} solo debe contener números y un signo '+' opcional al inicio`);
   }
 } module.exports = validation;
