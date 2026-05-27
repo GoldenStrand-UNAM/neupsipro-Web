6479e4063130138a1d7182799628b7d750d051ef
Andrey <enriqueandrey@hotmail.com>
2026-05-27 00:39:13 -0600

    feat: wais endpoint

---
+++ Back/src/presentation/controller/testApplications/postWais.controller.js
@@ -0,0 +1,44 @@
+class postWaisController {
+
+  constructor (postWaisUseCase) {
+    this.useCase = postWaisUseCase;
+  }
+
+  async postResult (req, res) {
+    try {
+      const { id_user, id_application } = req.params;
+
+      const {
+        score_com_verbal,
+        score_razon_perceptual,
+        score_mem_work,
+        score_velo_proce,
+        score_total,
+        notes,
+      } = req.body;
+
+      const dto = await this.useCase.execute({
+        id_user,
+        id_application,
+        score_com_vocal: score_com_verbal,
+        score_com_verbal,
+        score_razon_perceptual,
+        score_mem_work,
+        score_velo_proce,
+        score_total,
+        notes,
+      });
+
+      return res.status(200).json({ data: dto });
+
+    } catch (err) {
+      if (err.status && err.message) {
+        return res.status(err.status).json({ error: err.message });
+      }
+      console.error('[postWaisController]', err);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+}
+
+module.exports = postWaisController;
