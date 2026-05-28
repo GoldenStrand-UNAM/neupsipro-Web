421ed1119dc19cab8050db818d74d8dc4262d320
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-05-28 16:17:21 -0600

    fix(protocol): manage errors

--- Back/src/application/usecase/users/modifyProtocolUseCase.js
+++ Back/src/application/usecase/users/modifyProtocolUseCase.js
@@ -1,15 +1,29 @@
-
 class modifyProtocolUseCase {
-  constructor (userRepository) {
+  constructor(userRepository) {
     this.userRepository = userRepository;
   }
 
-  async execute ({ id_user, protocol }) {
+  async execute({ id_user, protocol }) {
 
-    const updated = await this.userRepository.editUserProtocol({
-      id_user,
-      protocol,
-    });
+    if (!id_user) {
+      throw new Error('id_user is required');
+    }
+
+    const user =
+      await this.userRepository.fetchOne({ id_user });
+
+    if (!user || user.length === 0) {
+      throw new Error('User not found');
+    }
+    if (!protocol) {
+      throw new Error('Protocol is required');
+    }
+
+    const updated =
+      await this.userRepository.editUserProtocol({
+        id_user,
+        protocol,
+      });
 
     return updated;
   }
