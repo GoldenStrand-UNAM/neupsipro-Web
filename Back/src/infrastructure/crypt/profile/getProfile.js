0040d862f9f82f0161fb34e167dbbaf98ceaa7c1
Frida Arcadia <frida.arcadialv@gmail.com>
2026-05-28 18:34:32 -0600

    feat(consultProfile): uncrypt data

---
+++ Back/src/infrastructure/crypt/profile/getProfile.js
@@ -0,0 +1,27 @@
+const Crypt = require('../crypt');
+
+const crypt = new Crypt();
+
+function safeDecrypt (value) {
+  if (!value) return value;
+  try {
+    return crypt.decrypt(value);
+  } catch (error) {
+    return value;
+  }
+}
+
+function uncryptData (data) {
+  return {
+    ...data,
+    first_name: safeDecrypt(data.first_name),
+    lastname_p: safeDecrypt(data.lastname_p),
+    lastname_m: safeDecrypt(data.lastname_m),
+    birthdate: safeDecrypt(data.birthdate),
+    neuro_entry_date: safeDecrypt(data.neuro_entry_date),
+    prosthetist: safeDecrypt(data.prosthetist),
+    assigned_clinic_name: safeDecrypt(data.assigned_clinic_name),
+  };
+}
+
+module.exports = uncryptData;
