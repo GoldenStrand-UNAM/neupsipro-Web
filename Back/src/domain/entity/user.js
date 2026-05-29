afeea4b1f5e87ad8a68011fe13fa727ad621ec38
Shaula Paz Garay <a01712013tec.mx>
2026-05-28 22:43:43 -0600

    fix(clinical-user): unneeded console logs

--- Back/src/domain/entity/user.js
+++ Back/src/domain/entity/user.js
@@ -1,6 +1,5 @@
 class User {
   constructor (data) {
-    console.log(data);
     this.idUser = data.id_user;
     this.photo = data.profile_photo;
     this.referenceNumber = data.reference_number;
