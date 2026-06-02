8199b6942149b370698d3df47746f46336c49139
MasterCrafter4 <r2d2.dany@gmail.com>
2026-06-01 20:01:18 -0600

    feat(test-applications): add test results csv export api

--- Back/src/domain/repository/testExportRepository.js
+++ Back/src/domain/repository/testExportRepository.js
@@ -1,4 +1,8 @@
 class testExportRepository {
+  async fetchAllResults() {
+    throw new Error('fetchAllResults() must be implemented');
+  }
+
   async fetchBanfeResults() {
     throw new Error('fetchBanfeResults() must be implemented');
   }
@@ -7,20 +11,8 @@ class testExportRepository {
     throw new Error('fetchWaisResults() must be implemented');
   }
 
-  async fetchMocaResults() {
-    throw new Error('fetchMocaResults() must be implemented');
-  }
-
-  async fetchNihResults() {
-    throw new Error('fetchNihResults() must be implemented');
-  }
-
   async fetchReyResults() {
     throw new Error('fetchReyResults() must be implemented');
-  }
-
-  async fetchAllResults() {
-    throw new Error('fetchAllResults() must be implemented');
   }
 }
 
