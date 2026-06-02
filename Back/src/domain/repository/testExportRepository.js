b3b3304e792f8bc0f0c33a35ee4b64085f747ce1
MasterCrafter4 <r2d2.dany@gmail.com>
2026-06-01 19:20:39 -0600

    feat(test-applications): add test results export use case

---
+++ Back/src/domain/repository/testExportRepository.js
@@ -0,0 +1,27 @@
+class testExportRepository {
+  async fetchBanfeResults() {
+    throw new Error('fetchBanfeResults() must be implemented');
+  }
+
+  async fetchWaisResults() {
+    throw new Error('fetchWaisResults() must be implemented');
+  }
+
+  async fetchMocaResults() {
+    throw new Error('fetchMocaResults() must be implemented');
+  }
+
+  async fetchNihResults() {
+    throw new Error('fetchNihResults() must be implemented');
+  }
+
+  async fetchReyResults() {
+    throw new Error('fetchReyResults() must be implemented');
+  }
+
+  async fetchAllResults() {
+    throw new Error('fetchAllResults() must be implemented');
+  }
+}
+
+module.exports = testExportRepository;
