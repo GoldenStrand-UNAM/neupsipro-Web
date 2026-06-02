294d7d8afed1d99ebb45ffd998146e0840263d7e
Goyeneche23 <juanjogoyeneche@gmail.com>
2026-06-02 16:26:31 -0600

    feat(peers): dto and entity

---
+++ Back/src/domain/entity/peerSession.js
@@ -0,0 +1,13 @@
+class PeerSession {
+  constructor (row = {}) {
+    this.id = row.id_peer_session;
+    this.title = row.title;
+    this.responsable = row.responsable;
+    this.note = row.note ?? null;
+    this.date = row.date;
+    this.menCount = Number(row.men_count) || 0;
+    this.womenCount = Number(row.women_count) || 0;
+  }
+}
+
+module.exports = PeerSession;
