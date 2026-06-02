db91ec4af50448ff7c50b63865866bbd0d33dfee
Jh700wolf <jh700wolf@gmail.com>
2026-06-02 16:08:24 -0600

    fix(physical-concerns): removes console logs in repository

--- Back/src/infrastructure/repositories/ImpPhysicalConcernsRepository.js
+++ Back/src/infrastructure/repositories/ImpPhysicalConcernsRepository.js
@@ -3,13 +3,11 @@ const PhysicalConcernsRepository = require('../../domain/repository/physicalConc
 
 class ImpPhysicalConcernsRepository extends PhysicalConcernsRepository {
   async fetchPhysicalConcerns ({ idUserRelation }) {
-    console.log(idUserRelation);
     const [rows] = await db.query(`
       SELECT headache, dizziness, urinary_inconsistency, skin_problem, id_user_relation
       FROM clinical_interview
       WHERE id_user_relation = ?;
     `, [idUserRelation]);
-    console.log(rows);
     return rows[0];
   }
 }
