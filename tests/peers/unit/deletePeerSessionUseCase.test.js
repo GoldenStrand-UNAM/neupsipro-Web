4e4136e0b3ddf87ca6abfe22ec728df29e93e632
Jh700wolf <jh700wolf@gmail.com>
2026-06-04 18:07:30 -0600

    test(peer-session): adds unit tests for delete peer session

---
+++ tests/peers/unit/deletePeerSessionUseCase.test.js
@@ -0,0 +1,30 @@
+const DeletePeerSessionUseCase = require('../../../Back/src/application/usecase/peers/deletePeerSessionUseCase');
+
+describe('DeletePeerSessionUseCase — Unit Tests', () => {
+    let mockPeerSessionRepository;
+    let useCase;
+
+    beforeEach(() => {
+      mockPeerSessionRepository = {
+            deleteSession: jest.fn(),
+        };
+        useCase = new DeletePeerSessionUseCase(mockPeerSessionRepository);
+    });
+
+    test('Happy Path, the session is deleted without errors', async() => {
+      mockPeerSessionRepository.deleteSession.mockResolvedValue({id_peer_session: "s-111"});
+      const result = await useCase.execute({idPeerSession: "s-111"});
+      expect(result).toBe({success: true});
+    })
+
+    test('The system sends no id for the session to delete', async() => {
+      await expect(useCase.execute({})).rejects.toThrow("El identificador de la sesión es obligatorio");
+    });
+
+    test('The database founds no id matching the session sent to delete it', async() => {
+      mockPeerSessionRepository.deleteSession.mockResolvedValue(null);
+      await expect(useCase.execute({ idPeerSession: "s-11" }))
+      .rejects.toThrow('La sesión no existe'); 
+    }) 
+
+})
